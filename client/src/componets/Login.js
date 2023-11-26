7fe4b69daed8f16ace2f7bc2fe8d0f66ca3c371b
isaacambi <128852825+isaacambi@users.noreply.github.com>
2023-11-26 20:02:16 +0100

    Update Login.js

--- client/src/componets/Login.js
+++ client/src/componets/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
   };
   const sendRequest = async (type = "login") => {
     const res = await axios
-      .post(`http://localhost:5000/api/users/${type}`, {
+      .post(`http://54.213.113.42:5000/api/users/${type}`, {
         name: inputs.name,
         email: inputs.email,
         password: inputs.password,
