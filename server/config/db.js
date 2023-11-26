7b17bbc458c59915cdfe363f3046651ed0827adf
isaacambi <128852825+isaacambi@users.noreply.github.com>
2023-11-26 20:52:16 +0100

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/BlogApp").then(()=>{
+mongoose.connect("mongodb://mongo:27017/BlogApp").then(()=>{
     console.log("connected!");
 }).catch((err)=>{
     console.log(err);
